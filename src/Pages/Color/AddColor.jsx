a7d1de6b3ef79216144192cf817ddf9ee156636a
CyberRoshan <roshanchaurasia990@gmail.com>
2024-08-24 20:58:37 +0530

    Add Color Picker

--- src/Pages/Color/AddColor.jsx
+++ src/Pages/Color/AddColor.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../../common/Sidebar";
 import Header from "../../common/Header";
 import Breadcrumb from "../../common/Breadcrumb";
 import Footer from "../../common/Footer";
+import { ChromePicker } from "react-color";
 
 export default function AddColor() {
   return (
@@ -37,31 +38,42 @@ export default function AddColor() {
                 <div class="mb-5">
                   <label
                     for="base-input"
-                    class="block mb-5 text-md font-medium text-gray-900"
+                    class="block mb-8 text-md font-medium text-gray-900"
                   >
                     Color Picker
                   </label>
-
-                  
-                  <input
-                    type="color"
-                    class="p-1 h-[100px] w-[250px] block bg-white border border-gray-200 cursor-pointer rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700"
-                    id="hs-color-input"
-                    value="#FF00F7"
-                    title="Choose your color"
-                  ></input>
+                  <ChromePicker />
+                  <br />
                 </div>
-                <div className="pe-5 ps-1"><span className="flex items-center gap-3">
-                Status :
-                <input id="link-radio" type="radio" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "></input>Active
-                <input id="link-radio" type="radio" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "></input>Deactive
-                </span></div> 
-                <button type="button" class="focus:outline-none my-10 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Select Color</button>
-           
+                <div className="pe-5 ps-1">
+                  <span className="flex items-center gap-3">
+                    Status :
+                    <input
+                      id="link-radio"
+                      type="radio"
+                      value=""
+                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "
+                    ></input>
+                    Active
+                    <input
+                      id="link-radio"
+                      type="radio"
+                      value=""
+                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "
+                    ></input>
+                    Deactive
+                  </span>
+                </div>
+                <button
+                  type="button"
+                  class="focus:outline-none my-10 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+                >
+                  Select Color
+                </button>
               </div>
             </div>
+            <Footer />
           </div>
-          <Footer />
         </div>
       </div>
     </section>
